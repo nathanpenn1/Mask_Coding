00bbc4c2ff5edd37de6a13bf1a799555341e2528
Raven_JF <45084213+RavenJF361@users.noreply.github.com>
2021-03-18 20:54:53 -0700

    Adding one button functionality
    Everything is placed on the LCDScreenOutput.ino file. Might put everything in a seperate .cpp and .h file later down the line.

---
+++ LCDScreenOutput_3_18_21/ScreenDraw.h
@@ -0,0 +1,18 @@
+#ifndef SCREENDRAW_H
+#define SCREENDRAW_H
+
+void screenSetup();
+
+void drawingF(int Cx, int Cy, int R, int G, int B, int textSize);
+
+void percentageOutput(int i);
+
+void printWarning(); // Raven 3/17/21
+
+void printUV(); // Raven 3/6/21
+
+void graphUV(); // Raven 3/6/21
+
+void read_y2(); // Raven 3/12/21
+
+#endif
