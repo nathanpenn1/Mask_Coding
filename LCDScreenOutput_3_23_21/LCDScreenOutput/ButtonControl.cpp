310d98eaa96f691ddbfb88552d47f785b19f759e
nathanpenn1 <nathan_penn4@yahoo.com>
2021-03-23 23:47:27 -0700

    added new libraries (Battery.h and ButtonControl.h) in order to clean and simplify the main code file. Implemented all of their functions within their corresponding cpp files

---
+++ LCDScreenOutput_3_23_21/LCDScreenOutput/ButtonControl.cpp
@@ -0,0 +1,65 @@
+#include "ButtonControl.h"
+#include <EasyButton.h>
+
+#define batteryPercentPin A7
+#define oneButtonPIN D2 // 3/16/21
+#define LEDtoTestOneButtonFunc D3 // 3/16/21
+
+bool buttonPressed = 0;
+bool buttonPressedFourSeconds = 0;
+
+int timePressed_foursec = 4000; // 4 seconds, or 4000 milliseconds
+//attachInterrupt(digitalPinToInterrupt(oneButtonPin), oneButtonISR, FALLING);
+EasyButton oneButton(oneButtonPIN);
+
+void buttonSetup()
+{
+  oneButton.begin(); // Initialize the button
+
+  oneButton.onPressed(oneTap);
+  oneButton.onPressedFor(timePressed_foursec, fourSecFunc); // Set up the button for a 4 second long press
+
+
+  if (oneButton.supportsInterrupt())
+  {
+    oneButton.enableInterrupt(oneButtonISR);
+    Serial.println("Settings interrupts with the oneButton");
+  }
+}
+
+void fourSecFunc()
+{
+  buttonPressedFourSeconds = 1;
+}
+
+void oneButtonISR()
+{
+  oneButton.read();// Updating button status
+}
+
+// Note to Self -- long press for 4 seconds is good, but cant work with a single tap. 
+
+
+void oneTap()
+{
+  buttonPressed = 1;
+}
+
+void updateButton()
+{
+  oneButton.update();
+}
+
+void checkButton()
+{
+  if (buttonPressed)
+  {
+    Serial.println("Tapped once");
+    buttonPressed = 0;
+  }
+  else if (buttonPressedFourSeconds)
+  {
+    Serial.println("Button held for four seconds");
+    buttonPressedFourSeconds = 0;
+  }
+}
---
+++ LCDScreenOutput_3_23_21/LCDScreenOutput/ButtonControl.h
@@ -0,0 +1,16 @@
+#ifndef BUTTONCONTROL_H
+#define BUTTONCONTROL_H
+
+void buttonSetup();
+
+void fourSecFunc();
+
+void oneButtonISR();
+
+void oneTap();
+
+void updateButton();
+
+void checkButton();
+
+#endif
