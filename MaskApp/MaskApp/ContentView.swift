ca1cdf3c3fbd3581fd79bdd7f3ab00fe547069e9
David Nikiforov <72281347+dnik4ov97@users.noreply.github.com>
2021-03-29 12:00:17 -0700

    Add files via upload

--- MaskApp/MaskApp/ContentView.swift
+++ MaskApp/MaskApp/ContentView.swift
@@ -4,74 +4,156 @@
 //
 //  Created by David Nikiforov on 2/12/21.
 //
-
 import SwiftUI
+import SwiftUICharts
+import Charts
 
 struct ContentView: View {
     @ObservedObject var bleManager = BLEManager()
     var body: some View {
-        VStack (spacing: 10) {
- 
-            Text("Bluetooth Devices")
-                .font(.largeTitle)
-                .frame(maxWidth: .infinity, alignment: .center)
-            List(bleManager.peripherals) { peripheral in
-                HStack {
-                    Text(peripheral.name)
-                    Spacer()
-                    Text(String(peripheral.rssi))
+        NavigationView {
+            VStack () {
+                Text("UV Mask")
+                    .font(.system(size: 70))
+                    .frame(maxWidth: .infinity, maxHeight: 200, alignment: .center)
+                    .foregroundColor(.red)
+                    .offset(y: -170)
+                Text(bleManager.percentageVal.description + "%\t")
+                    .font(.system(size: 40))
+                    .frame(maxWidth: .infinity, maxHeight: 100,alignment: .center)
+                    .foregroundColor(.red)
+                    .offset(y: -100)
+                
+                HStack () {
+                    Text("UV Light")
+                        .font(.system(size: 20))
+                        .frame(maxWidth: .infinity, alignment: .center)
+                        .foregroundColor(.green)
+                    Text("Charger")
+                        .font(.system(size: 20))
+                        .frame(maxWidth: .infinity, alignment: .center)
+                        .foregroundColor(.green)
                 }
-            }.frame(height: 300)
- 
-            Spacer()
- 
-            Text("STATUS")
-                .font(.headline)
- 
-            // Status goes here
-            //Text("Bluetooth status here")
-                //.foregroundColor(.red)
-            if bleManager.isSwitchedOn {
-                Text("Bluetooth is switched on")
+                .offset(y: -70)
+                
+                HStack() {
+                    Spacer(minLength: 50)
+                    
+                    Text("ON")
+                        .font(.system(size: 40))
+                        .frame(minWidth: 80, minHeight: 60,alignment: .center)
+                        .foregroundColor(.white)
+                        .background(Color.green)
+                        .cornerRadius(10)
+                    
+                    Spacer(minLength: 115)
+                    
+                    Text("ON")
+                        .font(.system(size: 40))
+                        .frame(minWidth: 80, minHeight: 60,alignment: .center)
+                        .foregroundColor(.white)
+                        .background(Color.green)
+                        .cornerRadius(10)
+                    
+                    Spacer()
+                }
+                .offset(y: -65)
+                
+                HStack (spacing: 120) {
+                    NavigationLink(destination: BatteryView()){
+                        Image("BatteryIcon")
+                            .accentColor(Color(.label))
+                    }
+                    NavigationLink(destination: UVIntesityView(bleManager1: bleManager)){
+                        Image("GraphIcon")
+                            .accentColor(Color(.label))
+                        
+                    }
+                }
+                .offset(y: -70)
+                //LineView(data: bleManager.yVal , title: "UV Intesity", legend: "(nm)")
+                //LineChartView(data: bleManager.yVal, title: "UV Intesity")
+                
+                if bleManager.isSwitchedOn {
+                    Text("Bluetooth is switched on")
+                        .foregroundColor(.green)
+                }
+                else {
+                    Text("Bluetooth is NOT switched on")
+                        .foregroundColor(.red)
+                }
+            }
+        }
+    }
+}
+
+struct BatteryView: View {
+    //@ObservedObject var bleManager = BLEManager()
+    var body: some View {
+        VStack() {
+            HStack () {
+                Text("V(capacity)")
+                    .font(.system(size: 20))
+                    .frame(maxWidth: .infinity, alignment: .center)
+                    .foregroundColor(.green)
+                Text("V(battery)")
+                    .font(.system(size: 20))
+                    .frame(maxWidth: .infinity, alignment: .center)
                     .foregroundColor(.green)
             }
-            else {
-                Text("Bluetooth is NOT switched on")
-                    .foregroundColor(.red)
-            }
- 
-            Spacer()
- 
-            HStack {
-                VStack (spacing: 10) {
-                    Button(action: {
-                        self.bleManager.startScanning()
-                    }) {
-                        Text("Start Scanning")
-                    }
-                    Button(action: {
-                        self.bleManager.stopScanning()
-                    })  {
-                        Text("Stop Scanning")
-                    }
-                }.padding()
- 
+            .offset(y: -200)
+            
+            HStack() {
                 Spacer()
- 
-                VStack (spacing: 10) {
-                    Button(action: {
-                        print("Start Advertising")
-                    }) {
-                        Text("Start Advertising")
-                    }
-                    Button(action: {
-                        print("Stop Advertising")
-                    }) {
-                        Text("Stop Advertising")
-                    }
-                }.padding()
+                
+                Text("6V")
+                    .font(.system(size: 40))
+                    .frame(minWidth: 80, minHeight: 60,alignment: .center)
+                    .foregroundColor(.white)
+                    .background(Color.green)
+                    .cornerRadius(10)
+                
+                Spacer(minLength: 115)
+                
+                Text("5V")
+                    .font(.system(size: 40))
+                    .frame(minWidth: 80, minHeight: 60,alignment: .center)
+                    .foregroundColor(.white)
+                    .background(Color.green)
+                    .cornerRadius(10)
+                
+                Spacer()
             }
-            Spacer()
+            .offset(y: -200)
+            
+            Text("Perecentage")
+                .font(.system(size: 20))
+                .frame(maxWidth: .infinity, alignment: .center)
+                .foregroundColor(.green)
+                .offset(y: -150)
+            
+            Text("83.33%")
+                .font(.system(size: 40))
+                .frame(minWidth: 80, minHeight: 60,alignment: .center)
+                .foregroundColor(.white)
+                .background(Color.green)
+                .cornerRadius(10)
+                .offset(y: -150)
+            
+            Text("Charging ON")
+                .font(.system(size: 45))
+                .frame(maxWidth: .infinity, alignment: .center)
+                .foregroundColor(.red)
+                .offset(y: -50)
+        }
+    }
+}
+
+struct UVIntesityView: View {
+    @ObservedObject var bleManager1 : BLEManager
+    var body: some View {
+        HStack() {
+            LineView(data: bleManager1.yVal , title: "UV Intesity", legend: "(nm)")
         }
     }
 }
@@ -79,5 +161,6 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .preferredColorScheme(.light)
     }
 }
